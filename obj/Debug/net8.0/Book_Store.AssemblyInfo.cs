c6e68cb973f8e15c9f40ac00475de89342c7d27b
Danny <thanhan1100@gmail.com>
2024-10-08 17:23:34 +0700

    Delete Image Folder

--- obj/Debug/net8.0/Book_Store.AssemblyInfo.cs
+++ obj/Debug/net8.0/Book_Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7802c6f32ab2370e495d6e5a587a4da3916dff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b4910d427c8fed6d5b57018e0c6cbf1a2d14d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
